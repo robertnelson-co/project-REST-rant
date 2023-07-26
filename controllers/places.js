78876a94327fe4764f547620aebbc2f4d4cd6af4
robertnelson-co <hello@robertnelson.co>
2023-07-26 15:46:13 -0400

    adding New place route

--- controllers/places.js
+++ controllers/places.js
@@ -17,6 +17,11 @@ router.get('/', (req, res) => {
     }]
     res.render('places/index', { places })
 })
+
+router.get('/new', (req, res) => {
+    res.render('places/new')
+}
+)
 //More code here in a moment
 
 module.exports = router

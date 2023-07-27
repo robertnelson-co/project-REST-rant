3b4e74cbc937d99eb002505684bd6eab6a70ee0c
robertnelson-co <hello@robertnelson.co>
2023-07-27 19:15:10 -0400

    Updated form page

--- controllers/places.js
+++ controllers/places.js
@@ -20,8 +20,16 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
     res.render('places/new')
-}
-)
+})
+
+router.get('/places/:id', (req, res) => {
+    res.render('/places/:id')
+})
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
+})
 //More code here in a moment
 
 module.exports = router
